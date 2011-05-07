9e6555c23cdf1a747ee5d5ca2ebe090b11cff169
Micah Butler <jgordon24@zcat.com>
2011-05-07 10:21:07 -0500

    first commit

---
+++ src/words.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+string name; // for the name exchange
+string word; // for entering the settings
+string playagain; // asking if the user wants to play again
+string back; // takes you back to the program after viewing the help...
+string change; // for the settings...
+string names; // for showing the names of names.txt
+string newword; // for writing a new secret word
+string secretw; // for guessing the secret word
+string gcheck; // for checking the guess
+string hnum; // for setting the high number in the game
+int guess; // for guessing the number in the game
+int highn; // for checking the high number
+int range; // I don't think I am using this one anymore
+int rando; // for the random number
+
+
