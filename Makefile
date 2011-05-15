57d8cb2e104cc46a7e763667385511f088f6c281
Micah Butler <jgordon24@zcat.com>
2011-05-15 17:15:30 -0500

    first commit

---
+++ Makefile
@@ -0,0 +1,18 @@
+# compiler to use:
+CC=g++
+# flags to use:
+CFLAGS=-c -Wall
+
+all: words
+
+words: src/words.o src/ccalc.o
+	$(CC) src/words.o src/ccalc.o -o src/copy/words
+
+words.o: src/words.cpp
+	$(CC) $(CFLAGS) src/words.cpp
+
+ccalc.o: src/ccalc.cpp
+	$(CC) $(CFLAGS) src/ccalc.cpp
+
+clean:
+	rm -rf src/*o
--- src/words.h
+++ src/words.h
@@ -1,3 +1,11 @@
+/*	First c++ program
+ *	Micah Butler (princessjinifer)
+ *	Lakshmipathi.G (Laks)
+ *	Sometime 2010
+ *	It has no functionality, it does nothing useful, just has a few things mostly related to the game...
+ *	v.0.2
+*/
+
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -5,21 +13,60 @@
 #include <sstream>
 #include <string>
 #include <cstring>
+#include <math.h>
+
 using namespace std;
 
-string name; // for the name exchange
-string word; // for entering the settings
-string playagain; // asking if the user wants to play again
-string back; // takes you back to the program after viewing the help...
-string change; // for the settings...
-string names; // for showing the names of names.txt
-string newword; // for writing a new secret word
-string secretw; // for guessing the secret word
-string gcheck; // for checking the guess
-string hnum; // for setting the high number in the game
-int guess; // for guessing the number in the game
-int highn; // for checking the high number
-int range; // I don't think I am using this one anymore
-int rando; // for the random number
+// int main
 
+extern string name; // for the name exchange
 
+// void quit
+
+extern string where_to; // getting around the program
+extern string confirm_quit; // confirmation of quitting the program
+
+// void play
+
+extern string gcheck; // for checking the guess
+extern int guess; // guessing the number in the game
+extern int score; // how many guesses that you have made
+extern int incorrect_guess; // how many times they guess something other than something in between 1 and highn
+
+// void settings
+
+extern string word; // the password on the settings
+extern string change; // in the settings, when it asks you what you want to do
+extern string newword; // when choosing a new password in the settings
+extern string secretw; // the password when entering the settings
+
+// void list
+
+extern string names; // extracts the names file to this
+
+// void extract
+
+extern string hnum; // extraction of the number file
+
+// void random_range
+
+extern int highn; // used to generate the random number
+extern int rando; // the random number
+
+// void you_win
+
+extern string playagain; // when you are asked if you want to play again
+
+// void highscores
+
+extern string highscore; // for displaying the highscores of the game
+
+// ccalc.cpp
+
+extern string measurement_type;
+extern string another_one;
+extern float measurement_length;
+extern float measurement_length_diameter;
+extern float measurement_length_circumference;
+extern float measurement_length_radius;
+extern float measurement_length_area;
